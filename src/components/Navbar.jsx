faea869dff95616a796d76326c9ac4984659b5f0
Milktea0408 <lucychen030408@gmail.com>
2025-05-15 00:45:03 +1000

    adding icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,48 @@
 import { useNavigate } from "react-router-dom";
+import linkedinIcon from "../assets/linkedin.svg";
+import emailIcon from "../assets/email.svg";
+import githubIcon from "../assets/github.svg";
+
 function Navbar() {
   const navigate = useNavigate();
-  
+
   function goToPage(page) {
-    navigate(page)
+    navigate(page);
   }
 
   return (
-    <nav className="fixed top-0 w-full bg-[#eee] text-black flex items-center justify-between px-8 py-4">
+    <nav className="fixed top-0 w-full bg-[#a9a9a9] text-white flex items-center justify-between px-8 py-4">
       {/* navlinks */}
-      <section className="flex gap-2">
-        <a onClick={() => goToPage("/home")}>Home</a>
-        <a onClick={() => goToPage("/contact")}>Contact</a>
-        <a onClick={() => goToPage("/about")}>About</a>
+      <section className="flex gap-3">
+        <a onClick={() => goToPage("/home")} className="cursor-pointer">Home</a>
+        <a onClick={() => goToPage("/contact")} className="cursor-pointer">Contact</a>
+        <a onClick={() => goToPage("/about")} className="cursor-pointer">About</a>
       </section>
-      
+
       {/* name in the middle */}
       <section>LUCY CHEN</section>
-      
+
       {/* socials */}
-      <section className="flex gap-2">
-        <a>Linkedin</a>
-        <a>Email</a>
-        <a>Github</a>
+      <section className="flex gap-3">
+        <a
+          className="cursor-pointer"
+          href="https://linkedin.com/in/lucy-c-791635216"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedinIcon} alt="LinkedIn" className="h-6 w-6" />
+        </a>
+        <a
+          className="cursor-pointer"
+          href="https://github.com/Milktea0408"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" className="h-6 w-6" />
+        </a>
+        <a className="cursor-pointer" href="mailto:lucychen030408@gmail.com">
+          <img src={emailIcon} alt="Email" className="h-6 w-6" />
+        </a>
       </section>
     </nav>
   );
