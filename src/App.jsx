7382c5ab2ba92a04e587b05857d510ed5f97f8ff
Milktea0408 <lucychen030408@gmail.com>
2025-05-14 23:27:49 +1000

    fixing tailwind issue and setting up navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import "./index.css";
-
 function App() {
-  return <>Hello world!</>;
+  return (
+    <h1 className="text-red-500 font-bold text-4xl underline">Hello world!</h1>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <>This is the navbar</>;
+}
+
+export default Navbar;
