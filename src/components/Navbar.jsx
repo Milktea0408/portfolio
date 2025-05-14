e95522f3f002c1cfd7d997e2a9892def4de90f08
Milktea0408 <lucychen030408@gmail.com>
2025-05-15 00:49:57 +1000

    adding hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,13 +14,13 @@ function Navbar() {
     <nav className="fixed top-0 w-full bg-[#a9a9a9] text-white flex items-center justify-between px-8 py-4">
       {/* navlinks */}
       <section className="flex gap-3">
-        <a onClick={() => goToPage("/home")} className="cursor-pointer">Home</a>
-        <a onClick={() => goToPage("/contact")} className="cursor-pointer">Contact</a>
-        <a onClick={() => goToPage("/about")} className="cursor-pointer">About</a>
+        <a onClick={() => goToPage("/home")} className="cursor-pointer hover:brightness-75 transition-all duration-100">Home</a>
+        <a onClick={() => goToPage("/contact")} className="cursor-pointer hover:brightness-75 transition-all duration-100">Contact</a>
+        <a onClick={() => goToPage("/about")} className="cursor-pointer hover:brightness-75 transition-all duration-100">About</a>
       </section>
 
       {/* name in the middle */}
-      <section>LUCY CHEN</section>
+      <section className="font-bold cursor-pointer hover:brightness-75 transition-all duration-100">LUCY CHEN</section>
 
       {/* socials */}
       <section className="flex gap-3">
@@ -30,7 +30,7 @@ function Navbar() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={linkedinIcon} alt="LinkedIn" className="h-6 w-6" />
+          <img src={linkedinIcon} alt="LinkedIn" className="h-6 w-6 hover:brightness-75 transition-all duration-100" />
         </a>
         <a
           className="cursor-pointer"
@@ -38,10 +38,10 @@ function Navbar() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={githubIcon} alt="GitHub" className="h-6 w-6" />
+          <img src={githubIcon} alt="GitHub" className="h-6 w-6 hover:brightness-75 transition-all duration-100" />
         </a>
         <a className="cursor-pointer" href="mailto:lucychen030408@gmail.com">
-          <img src={emailIcon} alt="Email" className="h-6 w-6" />
+          <img src={emailIcon} alt="Email" className="h-6 w-6 hover:brightness-75 transition-all duration-100" />
         </a>
       </section>
     </nav>
