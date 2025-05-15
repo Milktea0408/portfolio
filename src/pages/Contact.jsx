24befa208d11a7333cda5c93775d575326a13f24
Milktea0408 <lucychen030408@gmail.com>
2025-05-15 18:45:38 +1000

    setting up contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,8 +5,53 @@ function Contact() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-grow mt-16 px-4 md:px-8">
-        <h1>Welcome to the Contact page!</h1>
+      <main className="flex-grow mt-28 px-4 md:px-8">
+        <h1 className="font-bold text-center text-5xl">Contact me</h1>
+        <form className="flex flex-col justify-items-center content-center max-w-sm mx-auto">
+          {/* Name input */}
+          <div className="mb-5">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          {/* Email input */}
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          {/* message box */}
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            rows="4"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Message"
+          ></textarea>
+        </form>
       </main>
       <Footer />
     </div>
