40d4d4a4fd25cb7c8d9f267baa267cda1e8e0fca
Milktea0408 <lucychen030408@gmail.com>
2025-05-15 18:55:13 +1000

    adding submit btn to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,12 +2,18 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function Contact() {
+
+  // function to submit the form
+  function submitForm() {
+
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow mt-28 px-4 md:px-8">
         <h1 className="font-bold text-center text-5xl">Contact me</h1>
-        <form className="flex flex-col justify-items-center content-center max-w-sm mx-auto">
+        <form className="flex flex-col justify-items-center content-center mt-11 max-w-sm mx-auto">
           {/* Name input */}
           <div className="mb-5">
             <label
@@ -51,6 +57,14 @@ function Contact() {
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Message"
           ></textarea>
+          {/* submit btn */}
+          <button
+            type="submit"
+            className="mt-4 bg-white text-black border border-black font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center transition-colors duration-300 hover:bg-gray-700 hover:text-white"
+            onClick={submitForm}
+          >
+            Submit
+          </button>
         </form>
       </main>
       <Footer />
