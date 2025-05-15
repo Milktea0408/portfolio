f74d7f8c1cc748d491e52b5809cbadbea3f690a0
Milktea0408 <lucychen030408@gmail.com>
2025-05-15 19:30:04 +1000

    setting up email sending functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,18 +2,20 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function Contact() {
-
   // function to submit the form
-  function submitForm() {
-
+  function submitForm(e) {
+    e.preventDefault();
   }
 
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-grow mt-28 px-4 md:px-8">
+      <main className="flex-grow mt-28 px-4 md:px-8 ">
         <h1 className="font-bold text-center text-5xl">Contact me</h1>
-        <form className="flex flex-col justify-items-center content-center mt-11 max-w-sm mx-auto">
+        <form
+          className="flex flex-col justify-items-center content-center mt-11 max-w-sm mx-auto"
+          onSubmit={submitForm}
+        >
           {/* Name input */}
           <div className="mb-5">
             <label
@@ -26,6 +28,7 @@ function Contact() {
               type="text"
               id="name"
               placeholder="Name"
+              required
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
@@ -41,6 +44,7 @@ function Contact() {
               type="text"
               id="email"
               placeholder="Email"
+              required
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
@@ -60,8 +64,8 @@ function Contact() {
           {/* submit btn */}
           <button
             type="submit"
+            required
             className="mt-4 bg-white text-black border border-black font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center transition-colors duration-300 hover:bg-gray-700 hover:text-white"
-            onClick={submitForm}
           >
             Submit
           </button>
