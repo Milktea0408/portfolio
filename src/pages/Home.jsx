c9073f03816c3ff693bb8c15c54bd4bdc946989c
Milktea0408 <lucychen030408@gmail.com>
2025-05-16 14:59:24 +1000

    adding tech stack sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+// programming languages icons
+import javascriptIcon from "../assets/javascript.svg";
 
 function Home() {
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
@@ -20,14 +21,74 @@ function Home() {
         {/* tech stack */}
         <section>
           <h1 className="font-bold text-3xl mb-5">Technical Skills</h1>
-          <h2 className="text-xl">Programming Languages</h2>
-          {/* js/ts, python, c, dash, */}
-          <h2 className="text-xl">Frontend</h2>
-          {/* react, html, css, bootstrap, tailwind, material ui */}
-          <h2 className="text-xl">Backend + Database</h2>
-          {/* postgresql, node.js */}
-        </section>
 
+          {/* js/ts, python, c, dash, */}
+          <h2 className="text-xl">Programming Languages</h2>
+          <div className="flex gap-5 flex-wrap my-7">
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="Javascript logo" src={javascriptIcon} />
+              <p>Javascript</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="TypeScript logo" src="temp.jpg" />
+              <p>Typescript</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="Python logo" src="temp.jpg" />
+              <p>Python</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="C logo" src="temp.jpg" />
+              <p>C</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="Java logo" src="temp.jpg" />
+              <p>Java</p>
+            </div>
+          </div>
+
+          {/* react, html, css, bootstrap, tailwind, material ui */}
+          <h2 className="text-xl">Frontend</h2>
+          <div className="flex gap-5 flex-wrap my-7">
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="React logo" src="temp.jpg" />
+              <p>React</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="HTML logo" src="temp.jpg" />
+              <p>HTML</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="CSS logo" src="temp.jpg" />
+              <p>CSS</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="BootStrap logo" src="temp.jpg" />
+              <p>BootStrap</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="Tailwind logo" src="temp.jpg" />
+              <p>Tailwind</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="Material UI logo" src="temp.jpg" />
+              <p>Material UI</p>
+            </div>
+          </div>
+
+          {/* postgresql, node.js */}
+          <h2 className="text-xl">Backend + Database</h2>
+          <div className="flex gap-5 flex-wrap my-7">
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="PostgreSQL logo" src="temp.jpg" />
+              <p>PostgreSQL</p>
+            </div>
+            <div className="w-32 h-32 bg-[#a9a9a9] rounded-lg flex flex-col items-center justify-center">
+              <img alt="NodeJS logo" src="temp.jpg" />
+              <p>NodeJS</p>
+            </div>
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
